efe8a40e34c4865e244c1334f54955a3fce65a7d
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-22 18:53:09 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
 #This will install flask with a version of 2.1.0
 package{'flaskg':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-#This will install Werkzeug with a version of 2.1.0
 package{'Werkzeug':
-  ensure => '2.1.1',
+  ensure   => '2.1.1',
   providwr => 'pip3',
 }
