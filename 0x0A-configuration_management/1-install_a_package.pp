b1a6a63c2a8e2c795d283156b79d25ba103390aa
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-22 19:22:50 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,9 @@ package{'flaskg':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
 #This will install werkzeug with a version of 2.1.1
 package{'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip3',
+  provider => 'pip3'
 }
