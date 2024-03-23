c78a5f201643cf5ec09779cd1435a29b18c14b57
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-03-22 21:16:15 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
-#This will install flask with a version of 2.1.0
+#This will install flask and werkzeug with their versions
 package{'flaskg':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-#This will install werkzeug with a version of 2.1.1
 package{'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3'
