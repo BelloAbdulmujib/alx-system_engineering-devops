ad852a93d6d9d959b6d2382a03b91e57762f3ff8
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-22 03:34:47 -1100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,14 +5,14 @@
  import requests as r
 
   if __name__ == "--main--":
-      url = 'https://jsonplaceholder.typicode.com/todos/1'
-      usr_id = r.get('users/{}'.format(sys.argv[1])).json()
-      to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-
-      completed = [title.get("title") for title in to_do if
+       url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
+    completed = [title.get("title") for title in to_do if
                  title.get('completed') is True]
-        print(completed)
-        print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
                                                           len(completed),
                                                           len(to_do)))
-        [print("\t {}".format(title)) for title in completed]
+    [print("\t {}".format(title)) for title in completed]
