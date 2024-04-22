fba26d23cf90175813e9c28a4c3fd304f5f13712
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-22 03:24:33 -1100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+ """This script used API to get information of a user and return the TODO list progress"""
+
+ import sys
+ import requests
+ import req
+
+  if __name__ == "--main--":
+      url = 'https://jsonplaceholder.typicode.com/todos/1'
+      usr_id = req.get('users/{}'.format(sys.argv[1])).json()
+      to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+
+      completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+        print(completed)
+        print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+        [print("\t {}".format(title)) for title in completed]
