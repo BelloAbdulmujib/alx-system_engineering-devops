9825830e77ea2122ebcc8bc69f931ec010cbcf9b
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-05-07 11:56:25 -1100

    0x16-api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" fuction that queries and returns number of subscribers """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ this returns numbers of subscribers """
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(breddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
