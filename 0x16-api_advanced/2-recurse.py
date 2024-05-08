faab10c91344440ad4c61898740050788cc8078b
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-05-07 13:24:54 -1100

    0x16-api_advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""this script Contains recurse function"""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """
-    funtionr eturns a list of titles of all hot posts on a given subreddit
-    """
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "after": after,
